8de0df173613b0719f082ae1ed0487e06b3386f2
Aubin Paul <outlyer@users.noreply.github.com>
2023-08-16 16:25:40 -0400

    Fix bash references

--- mesonInstaller.sh
+++ mesonInstaller.sh
@@ -1,16 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cp -rf "${MESON_SOURCE_ROOT}"/PathOfBuildingBuild/* "${MESON_INSTALL_PREFIX}"/Contents/MacOS/
 cp -f "${MESON_SOURCE_ROOT}"/fonts/*.ttf "${MESON_INSTALL_PREFIX}"/Contents/MacOS/
 
 mkdir -p "${MESON_INSTALL_PREFIX}/Contents/Frameworks"
-
-# Meson doesn't bundle dependencies of our shared library by default, so we have
-# to do it on our own.
-#
-# This fixes the error:
-# "Symbol not found: _curl_easy_option_by_id" since that didn't exist in older
-# cURL libraries.
-#echo 'Bundling dylibs for lcurl.so'
-#cd "${MESON_INSTALL_PREFIX}/Contents"
-#dylibbundler --overwrite-dir --create-dir --bundle-deps --fix-file MacOS/lcurl.so
